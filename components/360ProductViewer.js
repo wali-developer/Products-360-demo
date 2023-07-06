5891837512816cb0278935e41a634b92b89e7517
Wali  Ullah <waliullah0540@gmail.com>
2023-07-07 00:46:52 +0500

    first commit

---
+++ components/360ProductViewer.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function ProductViewer360() {
+    return (
+        <View>
+            <Text>ProductViewer360</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({});
